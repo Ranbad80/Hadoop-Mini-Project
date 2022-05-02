2883ae2de61ed38412d9c6b9a12af2c267c9c942
Rania Badr <83798130+Ranbad80@users.noreply.github.com>
2022-05-02 12:11:54 -0700

    Add files via upload

---
+++ final_mapper2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+for line in sys.stdin:
+    # Remove any spaces
+    line = line.strip()
+    # Assign key value from output of reducer1.py into variables
+    key, value = line.split("\t")
+    print("%s\t%s"%(key, value))
